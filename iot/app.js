1ad1285c6a9ddcf71ea79f2478fe330cfce3db19
Sidharth R <68782815+sid-r-singh@users.noreply.github.com>
2020-11-16 19:49:50 +0530

    firebase config

--- iot/app.js
+++ iot/app.js
@@ -6,19 +6,6 @@ var percent=cnt.innerText;
 var prev_value=0;
 var interval;
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBFhmFjnzZslcHs7rJFKoHJQrgzfTVYVeY",
-  authDomain: "mt-iot-brn.firebaseapp.com",
-  databaseURL: "https://mt-iot-brn.firebaseio.com",
-  projectId: "mt-iot-brn",
-  storageBucket: "mt-iot-brn.appspot.com",
-  messagingSenderId: "414738283359",
-  appId: "1:414738283359:web:6c7f71b4c0db262e9dd4ea",
-  measurementId: "G-36H6XGSRE4"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 var database = firebase.database();
 var ref = database.ref("Tanks/tank1");
 var ref2 = database.ref("Tanks/tank2");
